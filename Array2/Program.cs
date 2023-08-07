835bdcf9baceece8a7a5996244b5b4087b5982e6
STUDIO 15 <STUDIO 15@DESKTOP-HFOS1SF>
2023-08-07 09:00:39 +0200

    Add project files.

---
+++ Array2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace Array2;
+class Program
+{
+
+    static void Main(string[] args)
+    {
+
+        int a = 4;
+        int b = 2;
+
+        try
+        {
+
+            Console.WriteLine("the anser is " + a / b);
+
+        }
+        catch
+        {
+
+            Console.WriteLine("an error has occured ");
+
+        }
+        finally
+        {
+
+            Console.WriteLine("finally has been called...");
+                
+        }
+    }
+
+}
